7be0b3a6c5d45f2a4f0669cf447c7cda140ea51c
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-02 19:06:17 -0700

    update our menu-items to navigate withRouter HOC

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import MenuItem from "../menu-item/menu-item.component";
-import "./directory.styles.scss";
+import MenuItem from "../MenuItem/MenuItem";
+import "./Directory.scss";
 
 class Directory extends Component {
   constructor() {
@@ -46,8 +46,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./homepage.styles.scss";
-import Directory from "../../components/directory/directory.component";
+import Directory from "../../components/Directory/Directory";
+import "./HomePage.scss";
 
 const HomePage = () => (
   <div className="homepage">
