a4b84e171c7765c6b4b48be5e8fb3a96d8d2ce86
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-05 21:38:38 -0700

    fixed redux-persist

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,12 +4,10 @@ import { connectAdvanced } from "react-redux";
 const selectCart = (state) => state.cart;
 //output selector
 
-export const selectCartItems = createSelector([selectCart], (cart) => {
-  console.log("cart==>>>>>", cart);
-  console.log("cart.cartItems==>>>>>", cart.items);
-
-  return cart.cartItems;
-});
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
 
 export const selectCartHidden = createSelector(
   [selectCart],
@@ -18,24 +16,17 @@ export const selectCartHidden = createSelector(
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  (cartItems) => {
-    console.log("cartItems==>", cartItems);
-    return cartItems.reduce(
+  (cartItems) =>
+    cartItems.reduce(
       (accumulatedQuantity, cartItem) =>
         accumulatedQuantity + cartItem.quantity,
       0
-    );
-  }
+    )
 );
-export const selectCartTotal = createSelector(
-  [selectCartItems],
-  (cartItems) => {
-    console.log("second cartItems", cartItems);
-
-    return cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity * cartItem.price,
-      0
-    );
-  }
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
