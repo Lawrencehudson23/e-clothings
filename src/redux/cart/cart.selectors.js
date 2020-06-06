b2c57b075c5dc4443167a9ff01e2ed95946b7df2
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-05 20:52:19 -0700

    problem is cartItems name should be items

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,12 +1,15 @@
 import { createSelector } from "reselect";
+import { connectAdvanced } from "react-redux";
 //NOTE: INPUT SELECTOR
 const selectCart = (state) => state.cart;
 //output selector
 
-export const selectCartItems = createSelector(
-  [selectCart],
-  (cart) => cart.cartItems
-);
+export const selectCartItems = createSelector([selectCart], (cart) => {
+  console.log("cart==>>>>>", cart);
+  console.log("cart.cartItems==>>>>>", cart.items);
+
+  return cart.cartItems;
+});
 
 export const selectCartHidden = createSelector(
   [selectCart],
@@ -15,17 +18,24 @@ export const selectCartHidden = createSelector(
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  (cartItems) =>
-    cartItems.reduce(
+  (cartItems) => {
+    console.log("cartItems==>", cartItems);
+    return cartItems.reduce(
       (accumulatedQuantity, cartItem) =>
         accumulatedQuantity + cartItem.quantity,
       0
-    )
+    );
+  }
 );
-export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
-  cartItems.reduce(
-    (accumulatedQuantity, cartItem) =>
-      accumulatedQuantity + cartItem.quantity * cartItem.price,
-    0
-  )
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => {
+    console.log("second cartItems", cartItems);
+
+    return cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity * cartItem.price,
+      0
+    );
+  }
 );
