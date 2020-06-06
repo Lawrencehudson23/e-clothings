1d321c159d6f77ddb611242ce050aa0ecff6e5d5
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-05 22:47:44 -0700

    added shop reducer, change data to local

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
-import SHOP_DATA from "./shop.data.js";
-
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
 
-const ShopPage = () => {
-  const [collections, setCollections] = useState(SHOP_DATA);
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...collections }) => (
+      <CollectionPreview key={id} {...collections} />
+    ))}
+  </div>
+);
 
-  return (
-    <div className="shop-page">
-      {collections.map(({ id, ...collections }) => (
-        <CollectionPreview key={id} {...collections} />
-      ))}
-    </div>
-  );
-};
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
