bc0681a637e6693a691cc1b2dd4487081db851fc
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-06 08:21:16 -0700

    cart drop down cants show cart item, debug

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 import "./CartItem.scss";
-const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">
-        {quantity} X ${price}
-      </span>
+const CartItem = ({ item }) => {
+  const { imageUrl, name, price, quantity } = item;
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} X ${price}
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CartItem;
