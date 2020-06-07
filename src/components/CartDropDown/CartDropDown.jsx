35c0f84f50eea5c29f6f5558007be95204633120
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-06 21:26:21 -0700

    removed redux-logger from production build

--- src/components/CartDropDown/CartDropDown.jsx
+++ src/components/CartDropDown/CartDropDown.jsx
@@ -7,30 +7,27 @@ import { withRouter } from "react-router-dom";
 import "./CartDropDown.scss";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
-const CartDropDown = ({ cartItems, history, dispatch }) => {
-  console.log("cartItems==>>", cartItems);
-  return (
-    <div className="cart-dropdown">
-      <div className="cart-items">
-        {cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} item={cartItem} />
-          ))
-        ) : (
-          <span className="empty-message">Your cart is empty</span>
-        )}
-      </div>
-      <CustomButton
-        onClick={() => {
-          history.push("/checkout");
-          dispatch(toggleCartHidden());
-        }}
-      >
-        GO TO CHECKOUT
-      </CustomButton>
+const CartDropDown = ({ cartItems, history, dispatch }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
     </div>
-  );
-};
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}
+    >
+      GO TO CHECKOUT
+    </CustomButton>
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
