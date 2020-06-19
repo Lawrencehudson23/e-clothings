cca4dcbda622d6baad68d90eed010cc368cfe33f
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-19 13:05:33 -0700

    finished cart action test

---
+++ client/src/redux/shop/shop.actions.test.js
@@ -0,0 +1,11 @@
+import shopActionTypes from "./shop.types";
+import {
+  fetchCollectionsStart,
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+  fetchCollectionsStartAsync,
+} from "./shop.actions";
+
+describe("fetchCollectionsStart action", () => {
+  it("");
+});
