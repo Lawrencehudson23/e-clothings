2c2c0d1fa5b3a15ed4ee14596aa41cd25ef28214
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-21 16:48:15 -0700

    add saga shop

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
-
 import rootReducer from "./root-reducer";
+import { fetchCollectionsStart } from "./shop/shop.sagas";
+//NOTE: saga replace thunk
+import createSagaMiddleware from "redux-saga";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
@@ -13,5 +15,7 @@ if (process.env.NODE_ENV === "development") {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+sagaMiddleware.run(fetchCollectionsStart);
+
 export const persistor = persistStore(store);
-// export default { store, persistStore };
+export default { store, persistStore };
