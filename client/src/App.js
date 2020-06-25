faa8f19f57427dfb3a76f1c91e52c3698ec48041
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-06-24 18:28:02 -0700

    lazy suspense spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 
-import HomePage from "./pages/HomePage/HomePage";
-import ShopPage from "./pages/ShopPage/ShopPage";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import Header from "./components/Header/Header";
-import LoginReg from "./pages/LoginReg/LoginReg";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 //NOTE: LESS CODE
 import { createStructuredSelector } from "reselect";
-import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
 import { checkUserSession } from "./redux/user/user.actions";
 import { GlobalStyle } from "./global.styles";
+import Header from "./components/Header/Header";
+import Spinner from "./components/Spinner/Spinner";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ShopPage = lazy(() => import("./pages/ShopPage/ShopPage"));
+const CheckoutPage = lazy(() => import("./pages/CheckoutPage/CheckoutPage"));
+const LoginReg = lazy(() => import("./pages/LoginReg/LoginReg"));
 
 const App = ({ currentUser, checkUserSession }) => {
-  // const unsubscribeFromAuth = null;
   useEffect(() => {
     checkUserSession();
-
-    //   // const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-    //   //   if (userAuth) {
-    //   //     const userRef = await createUserProfileDocument(userAuth);
-
-    //   //     userRef.onSnapshot((snapShot) => {
-    //   //       setCurrentUser({
-    //   //         id: snapShot.id,
-    //   //         ...snapShot.data(),
-    //   //       });
-    //   //     });
-    //   //   }
-
-    //   //   setCurrentUser(userAuth);
-    //   // });
-
-    //   //NOTE: mimic componentWillUnMount
-    return () => {
-      // unsubscribeFromAuth = null;
-    };
   }, [checkUserSession]);
 
   return (
@@ -44,14 +25,16 @@ const App = ({ currentUser, checkUserSession }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={CheckoutPage} />
-        <Route
-          exact
-          path="/login"
-          render={() => (currentUser ? <Redirect to="/" /> : <LoginReg />)}
-        />
+        <Suspense fallback={<Spinner />}>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/login"
+            render={() => (currentUser ? <Redirect to="/" /> : <LoginReg />)}
+          />
+        </Suspense>
       </Switch>
     </div>
   );
