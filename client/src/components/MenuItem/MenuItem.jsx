c67c9dcec80406018aa99be788a0392d813363ac
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-07-18 16:14:36 -0700

    added menuitem style component

--- client/src/components/MenuItem/MenuItem.jsx
+++ client/src/components/MenuItem/MenuItem.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import "./MenuItem.scss";
 
 //NOTE: withRouter => super component
 import { withRouter } from "react-router-dom";
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle,
+} from "./MenuItem.styles";
 function MenuItem({ title, imageUrl, history, size, linkUrl, match }) {
   return (
-    <div
-      className={`${size} menu-item`}
+    <MenuItemContainer
+      size={size}
       onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
-      <div
-        className="background-image"
+      <BackgroundImageContainer
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
+      <ContentContainer>
+        <ContentTitle>{title.toUpperCase()}</ContentTitle>
+        <ContentSubtitle>SHOP NOW</ContentSubtitle>
+      </ContentContainer>
+    </MenuItemContainer>
   );
 }
 export default withRouter(MenuItem);
--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -21,14 +21,14 @@ export const INITIAL_STATE = {
     {
       title: "womens",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
+      // size: "large",
       id: 4,
       linkUrl: "shop/womens",
     },
     {
       title: "mens",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
+      // size: "large",
       id: 5,
       linkUrl: "shop/mens",
     },
